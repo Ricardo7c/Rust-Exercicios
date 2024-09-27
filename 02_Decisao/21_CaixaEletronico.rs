82ebb834048230d4e5e26b8f0fa07dabeeefb311
Ricardo Silva <ricardo7c@hotmail.com>
2024-09-26 23:11:56 -0300

    Exercicio 21 concluido

---
+++ 02_Decisao/21_CaixaEletronico.rs
@@ -0,0 +1,62 @@
+use std::io::{self, Write};
+
+fn input_float(texto: &str) -> f32{
+    print!("{}",texto);
+    io::stdout().flush().unwrap();
+    let mut x = String::new();
+    io::stdin().read_line(&mut x).unwrap();
+    let x:f32 = x.trim().parse().unwrap();
+    x
+}
+
+fn main(){
+    let mut saque = input_float("Digite o valor do saque: ");
+    let mut n100 = 0;
+    let mut n50 = 0;
+    let mut n10 = 0;
+    let mut n5 = 0;
+    let mut n1 = 0;
+
+    while saque >= 100.0 {
+        n100 += 1;
+        saque -= 100.0;
+    }
+
+    while saque >= 50.0 {
+        n50 += 1;
+        saque -= 50.0;
+    }
+    
+    while saque >= 10.0 {
+        n10 += 1;
+        saque -= 10.0;
+    }
+    
+    while saque >= 5.0 {
+        n5 += 1;
+        saque -= 5.0;
+    }
+    
+    while saque >= 1.0 {
+        n1 += 1;
+        saque -= 1.0;
+    }
+
+
+    if n100 > 0 {
+        println!("{} Notas de R$100,00", n100);
+    }
+    if n50 > 0 {
+        println!("{} Notas de R$50,00", n50);
+    }
+    if n10 > 0 {
+        println!("{} Notas de R$10,00", n10);
+    }
+    if n5 > 0 {
+        println!("{} Notas de R$5,00", n5);
+    }
+    if n1 > 0 {
+        println!("{} Notas de R$1,00", n1);
+    }
+
+}
