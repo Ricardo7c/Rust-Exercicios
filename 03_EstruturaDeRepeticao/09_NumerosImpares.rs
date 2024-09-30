1c8b57102df873d86a311c6649e3a67f6a9b4b19
Ricardo Silva <ricardo7c@hotmail.com>
2024-09-30 16:41:51 -0300

    Finalizado!

--- 03_EstruturaDeRepeticao/09_NumerosImpares.rs
+++ 03_EstruturaDeRepeticao/09_NumerosImpares.rs
@@ -1,7 +1,7 @@
 // Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
 
 fn main(){
-    for cada in 0..50{
+    for cada in 0..50 {
         if cada % 2 != 0{
             println!("{}", cada);
         }
