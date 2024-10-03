43f6b1bfa50d273b276dda87178f5c0c8ca9c1b5
Ricardo Silva <ricardo7c@hotmail.com>
2024-10-03 14:10:17 -0300

    Finalizado!

--- 04_Listas/11_intercalar3vetores.rs
+++ 04_Listas/11_intercalar3vetores.rs
@@ -1,14 +1,16 @@
+
 fn main(){
     let numeros = vec![2, 3, 8, 9, 4, 12, 3, 4, 7, 5];
     let letras = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+    let simbolos = vec!['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
     let mut misto = Vec::new();
 
     for indice in 0..numeros.len(){
-        let tupla = (numeros[indice], letras[indice]);
+        let tupla = (numeros[indice], letras[indice], simbolos[indice]);
         misto.push(tupla);
     }
-    
+
     for tupla in misto{
-        print!(" {} {}", tupla.0, tupla.1);
+        print!(" {} {} {}", tupla.0, tupla.1, tupla.2);
     }
 }
