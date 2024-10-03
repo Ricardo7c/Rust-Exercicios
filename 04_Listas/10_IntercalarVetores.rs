f54c1bf8a20e8eeff8348364c26817922533b4fd
Ricardo Silva <ricardo7c@hotmail.com>
2024-10-03 13:48:28 -0300

    Finalizado!

--- 04_Listas/10_IntercalarVetores.rs
+++ 04_Listas/10_IntercalarVetores.rs
@@ -1,7 +1,10 @@
 fn main(){
-    let numeros = [2, 3, 8, 9, 4, 12, 3, 4, 7, 5];
-    let letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+    let numeros = vec![2, 3, 8, 9, 4, 12, 3, 4, 7, 5];
+    let letras = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
 
     let misto : Vec<_>= numeros.iter().zip(letras.iter()).collect();
-    print!("{:?}", misto);
+    
+    for cada in misto{
+        print!(" {} {}", cada.0, cada.1);
+    }
 }
