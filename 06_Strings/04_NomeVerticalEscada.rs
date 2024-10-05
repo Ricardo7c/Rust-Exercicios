1be8345172e2a435d99a120b17b4cd2ee858d27a
Ricardo Silva <ricardo7c@hotmail.com>
2024-10-05 08:01:51 -0300

    Finalizado!

---
+++ 06_Strings/04_NomeVerticalEscada.rs
@@ -0,0 +1,16 @@
+use std::io::{self, Write};
+
+fn input(texto: &str) -> String{ 
+    print!("{}", texto);
+    io::stdout().flush().unwrap();
+    let mut x = String::new();
+    io::stdin().read_line(&mut x).unwrap();
+    x
+}
+
+fn main(){
+    let nome = input("Digite seu nome: ").to_uppercase();
+    for cada in 1..=nome.trim().len(){
+        println!("{}", &nome[..cada])
+    }
+}
