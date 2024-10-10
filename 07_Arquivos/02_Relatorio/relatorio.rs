816035cd207989dd60b8fa677fb677aa450938c0
Ricardo Silva <ricardo7c@hotmail.com>
2024-10-10 12:13:44 -0300

    Finalizado!

---
+++ 07_Arquivos/02_Relatorio/relatorio.rs
@@ -0,0 +1,20 @@
+use std::fs::File;
+
+// fn ler_arquivo(arquivo: &str) -> Vec<[]>{
+//     let mut nome = String::new();
+//     let mut tamanho = String::new();
+//     let mut x = String::new();
+//     File::open(arquivo).unwrap().read_to_string(&mut x).unwrap();
+
+//     for cada in x.chars(){
+//         println!("{}",cada);
+//     }
+
+
+
+// }
+
+
+fn main() {
+    println!("Hello, world!");
+}
