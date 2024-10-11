51c5b4fba170157dea624d4442377473d02f082e
Ricardo Silva <ricardo7c@hotmail.com>
2024-10-11 01:17:57 -0300

    Finalizado!

--- 01_Sequencial/03_Soma.rs
+++ 01_Sequencial/03_Soma.rs
@@ -1,21 +1,18 @@
 use std::io::{self, Write};
 
-fn input(texto:&str) -> String{
+fn int(texto:&str) -> i32{
     print!("{texto}");
     io::stdout().flush().expect("Erro ao limpar");
     let mut x = String::new();
     io::stdin().read_line(&mut x).expect("Valor invalido");
+    let x = x.trim().parse::<i32>().unwrap();
     x
-}
-fn int(input:String) -> i32{
-    let num = input.trim().parse::<i32>().unwrap();
-    num
 }
 
 fn main(){
 
-    let num1 = int(input("Digite um numero: "));
-    let num2 = int(input("Digite outro numero: "));
+    let num1 = int("Digite um numero: ");
+    let num2 = int("Digite outro numero: ");
     println!("A soma de {} e {} é {}", num1, num2, (num1+num2));
     
 }
--- 01_Sequencial/04_Media.rs
+++ 01_Sequencial/04_Media.rs
@@ -1,22 +1,19 @@
 use std::io::{self, Write};
 
-fn input(texto:&str) -> String{
+fn float(texto:&str) -> f32{
     print!("{texto}");
     io::stdout().flush().expect("Erro ao limpar!");
     let mut x = String::new();
     io::stdin().read_line(&mut x).expect("Valor invalido!");
+    let x = x.trim().parse::<f32>().unwrap();
     x
-}
-fn float(input:String) -> f32{
-    let num = input.trim().parse::<f32>().unwrap();
-    num
 }
 
 fn main() {
-    let nota1 = float(input("Digite a primeira nota: "));
-    let nota2 = float(input("Digite a segunda nota: "));
-    let nota3 = float(input("Digite a terceira nota: "));
-    let nota4 = float(input("Digite a quarta nota: "));
+    let nota1 = float("Digite a primeira nota: ");
+    let nota2 = float("Digite a segunda nota: ");
+    let nota3 = float("Digite a terceira nota: ");
+    let nota4 = float("Digite a quarta nota: ");
     let media = (nota1+nota2+nota3+nota4)/4.0;
     println!("A media foi: {:.1}", media);
     
